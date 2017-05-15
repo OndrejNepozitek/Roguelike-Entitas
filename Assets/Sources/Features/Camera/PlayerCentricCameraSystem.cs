73c2ed503b4fa41e9f922172a5c1d75220644644
Ondřej Nepožitek <ondra@nepozitek.cz>
2017-05-15 20:46:10 +0200

    Smooth camera

--- Assets/Sources/Features/Camera/PlayerCentricCameraSystem.cs
+++ Assets/Sources/Features/Camera/PlayerCentricCameraSystem.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using Entitas;
 using UnityEngine;
 
-public sealed class PlayerCentricCameraSystem : ReactiveSystem<GameEntity>, IInitializeSystem
+public sealed class PlayerCentricCameraSystem : IExecuteSystem, IInitializeSystem
 {
     GameContext context;
     GameEntity camera;
     GameEntity player;
     IntVector2 offset;
 
-    public PlayerCentricCameraSystem(Contexts contexts) : base(contexts.game)
+    public PlayerCentricCameraSystem(Contexts contexts)
     {
         context = contexts.game;
         player = context.playerEntity;
@@ -25,23 +25,23 @@ public sealed class PlayerCentricCameraSystem : ReactiveSystem<GameEntity>, IIni
 
     private void UpdatePosition()
     {
-        Vector3 pos = (Vector2)(player.position.value + offset);
+        Vector3 pos;
+
+        if (player.hasView)
+        {
+            pos = (player.view.gameObject.transform.position + (Vector3)offset);
+        } else
+        {
+            pos = (Vector3)(player.position.value + offset);
+        }
+
         pos.z = -5;
         camera.camera.value.transform.position = pos;
+       
     }
 
-    protected override void Execute(List<GameEntity> entities)
+    public void Execute()
     {
         UpdatePosition();
-    }
-
-    protected override bool Filter(GameEntity entity)
-    {
-        return entity.isPlayer;
-    }
-
-    protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
-    {
-        return context.CreateCollector(GameMatcher.Position); // TODO: maybe overkill
     }
 }
